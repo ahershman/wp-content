8864f9ee530eb208f43332557173988cdf98922f
Aaron Hershman <aaron@kminot.com>
2014-07-04 11:23:27 -0700

    major restructure

--- themes/DaVinci/header.php
+++ themes/DaVinci/header.php
@@ -5,20 +5,25 @@
  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
  
+ <link href="header.css" rel="stylesheet" type="text/css">
+ 
 </head>
 
 <body>
   <header id="header">
     <div id="logo"></div>
     <div id="headimage"></div>
-    <nav id="mainnav">
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Portfolio</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Login</a></li>
-      </ul>
-    </nav>
+    <div id="nav">Content for  id "nav" Goes Here
+   		 <div id="mainnav">
+      		<ul>
+        		<li><a href="#">Home</a></li>
+        		<li><a href="#">Portfolio</a></li>
+				<li><a href="#">About</a></li>
+        	<li><a href="#">Login</a></li>
+      		</ul>
+    	</div>
+    	<div id="navicons">Content for  id "navicons" Goes Here</div>
+   </div>
   </header>
 </body>
 
