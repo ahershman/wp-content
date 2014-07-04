26b781d97de000b4fe311bc92f8cdb5ef04ed562
Aaron Hershman <aaron@kminot.com>
2014-07-04 15:59:45 -0700

    fixed header

--- themes/DaVinci/header.php
+++ themes/DaVinci/header.php
@@ -1,11 +1,10 @@
 <html>
 <head>
- <title>Tutorial theme</title>
+ <title>header</title>
  <script type="text/javascript" src="//use.typekit.net/bce7uaq.js"></script>
  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
- <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+ <link rel="stylesheet" href="<?php print get_stylesheet_directory_uri().'/CSS/header.css'; ?>">
  
- <link href="header.css" rel="stylesheet" type="text/css">
  
 </head>
 
@@ -13,16 +12,16 @@
   <header id="header">
     <div id="logo"></div>
     <div id="headimage"></div>
-    <div id="nav">Content for  id "nav" Goes Here
-   		 <div id="mainnav">
-      		<ul>
+    <div id="nav">
+      <div id="textnav">
+   		 <ul>
         		<li><a href="#">Home</a></li>
         		<li><a href="#">Portfolio</a></li>
 				<li><a href="#">About</a></li>
         	<li><a href="#">Login</a></li>
       		</ul>
     	</div>
-    	<div id="navicons">Content for  id "navicons" Goes Here</div>
+    	<div id="iconnav">Content for  id "navicons" Goes Here</div>
    </div>
   </header>
 </body>
