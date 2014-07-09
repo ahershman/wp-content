97fc9eec27db1e1d49ebee5d5ccb516f7373defc
Aaron Hershman <aaron@kminot.com>
2014-07-09 13:28:24 -0700

    live header and home page

--- themes/DaVinci/header.php
+++ themes/DaVinci/header.php
@@ -1,10 +1,10 @@
 <html>
 <head>
  <title>DaVinci Maps</title>
+ <!-- javascipt links here :will consolidate into function.php -->
  <script type="text/javascript" src="//use.typekit.net/bce7uaq.js"></script>
  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
- <link rel="stylesheet" href="<?php print get_stylesheet_directory_uri().'/CSS/header.css'; ?>">
- 
+
  <script>
 	  		function geth() {
 			// get proper node
@@ -19,22 +19,29 @@
 					
 </script>
 
+ <!-- All stysheet links go here : will be consolidated into style.css -->
+ <link rel="stylesheet" href="<?php print get_stylesheet_directory_uri().'/CSS/Common.css'; ?>">
+ <link rel="stylesheet" href="<?php print get_stylesheet_directory_uri().'/CSS/header.css'; ?>">
+ <link rel="stylesheet" href="<?php print get_stylesheet_directory_uri().'/CSS/home.css'; ?>">
+ 
  
 </head>
 
 <body>
-  <header id="header">
-    <div id="logo"></div>
-    <div id="headimage"></div>
-    <div id="nav">
-      <div id="textnav">
-   		 <ul>
-        		<li><a href="#">Home</a></li>
-        		<li><a href="#">Portfolio</a></li>
-				<li><a href="#">About</a></li>
-        	<li><a href="#">Login</a></li>
-      		</ul>
-    	</div>
-    	<div id="iconnav">Content for  id "navicons" Goes Here</div>
-   </div>
-  </header>
+	<header id="header">
+		<div id="logo"></div>
+		<div id="headimage"></div>
+		<div id="nav">
+			<div id="textnav">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Portfolio</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Login</a></li>
+				</ul>
+			</div><!-- textnav -->
+			<div id="iconnav"><img src="<?php print get_stylesheet_directory_uri().'/images/socialMediaIcons-01_03.png'; ?>"
+								 width="209" height="33" alt=""/></div>
+		</div><!-- nav-->
+	</header>
+<!-- body and html closing tags are in footer.php-->
